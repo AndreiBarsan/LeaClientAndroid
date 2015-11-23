898a338ff876e625abab865ec817d33ee7c27ec6
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2015-11-24 00:56:50 +0100

    Dicked around with Android Studio.

--- app/src/main/java/eu/cs_syd/leaclient/MainActivity.java
+++ app/src/main/java/eu/cs_syd/leaclient/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +18,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Dick butt");
+
+        Toast.makeText(getApplicationContext(), "Startup toast.", Toast.LENGTH_LONG).show();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "This block will encode the JSON and fire it off to Lea",
+                        Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
+                Toast.makeText(getApplicationContext(), "I am toast.", Toast.LENGTH_SHORT).show();
             }
         });
     }
