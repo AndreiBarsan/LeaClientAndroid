7928cefb70d8e9b98a8f14aa7c081bcbc34eeade
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2015-11-24 01:15:53 +0100

    Basic JSON reading.

--- app/src/main/java/eu/cs_syd/leaclient/MainActivity.java
+++ app/src/main/java/eu/cs_syd/leaclient/MainActivity.java
@@ -1,16 +1,37 @@
 package eu.cs_syd.leaclient;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
+
+    private String readTextAsset(String path) throws IOException {
+        try(InputStream rIs = getAssets().open(path);
+            BufferedReader br = new BufferedReader(new InputStreamReader(rIs))) {
+            String content = "";
+            String buffer;
+            while (null != (buffer = br.readLine())) {
+                content += buffer;
+            }
+
+            return content;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +39,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Dick butt");
+        getSupportActionBar().setTitle("Lea Client");
+
+        try {
+            String requestSchema = readTextAsset("json/request.json");
+            String replySchema = readTextAsset("json/reply.json");
+
+            
+
+        } catch(IOException e) {
+            e.printStackTrace();
+            Log.wtf("Assets", "Could not read JSON schema(s).");
+        }
 
         Toast.makeText(getApplicationContext(), "Startup toast.", Toast.LENGTH_LONG).show();
 
