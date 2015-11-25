fbe5c970ba8734eca6244d66c2a390a803db2055
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2015-11-25 20:28:14 +0100

    Reformat MainActivity.java.

--- app/src/main/java/eu/cs_syd/leaclient/MainActivity.java
+++ app/src/main/java/eu/cs_syd/leaclient/MainActivity.java
@@ -22,75 +22,74 @@ import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String readTextAsset(String path) throws IOException {
-        try(InputStream rIs = getAssets().open(path);
-            BufferedReader br = new BufferedReader(new InputStreamReader(rIs))) {
-            String content = "";
-            String buffer;
-            while (null != (buffer = br.readLine())) {
-                content += buffer;
-            }
+  private String readTextAsset(String path) throws IOException {
+    try (InputStream rIs = getAssets().open(path);
+         BufferedReader br = new BufferedReader(new InputStreamReader(rIs))) {
+      String content = "";
+      String buffer;
+      while (null != (buffer = br.readLine())) {
+        content += buffer;
+      }
 
-            return content;
-        }
+      return content;
+    }
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setTitle("Lea Client");
+
+    try {
+      String requestSchema = readTextAsset("json/request.json");
+      String replySchema = readTextAsset("json/reply.json");
+
+
+    } catch (IOException e) {
+      e.printStackTrace();
+      Log.wtf("Assets", "Could not read JSON schema(s).");
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Lea Client");
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Snackbar.make(view, "This block will encode the JSON and fire it off to Lea",
+          Snackbar.LENGTH_LONG).setAction("Action", null).show();
 
-        try {
-            String requestSchema = readTextAsset("json/request.json");
-            String replySchema = readTextAsset("json/reply.json");
+        String command = ((TextView) findViewById(R.id.commandInput)).getText().toString();
 
+        Toast.makeText(
+          getApplicationContext(),
+          "Command: " + command,
+          Toast.LENGTH_SHORT
+        ).show();
+      }
+    });
+  }
 
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
 
-        } catch(IOException e) {
-            e.printStackTrace();
-            Log.wtf("Assets", "Could not read JSON schema(s).");
-        }
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "This block will encode the JSON and fire it off to Lea",
-                        Snackbar.LENGTH_LONG).setAction("Action", null).show();
-
-                String command = ((TextView)findViewById(R.id.commandInput)).getText().toString();
-
-                Toast.makeText(
-                    getApplicationContext(),
-                    "Command: " + command,
-                    Toast.LENGTH_SHORT
-                ).show();
-            }
-        });
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    return super.onOptionsItemSelected(item);
+  }
 }
